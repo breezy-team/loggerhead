977b460b2569151d9e63b76c5a959459aee5c1b1
Robey Pointer <robey@lag.net>
2006-12-23 14:29:05 -0500

    add setup.py info so an sdist tarball can be made

--- loggerhead/release.py
+++ loggerhead/release.py
@@ -1,14 +1,12 @@
 # Release information about loggerhead
 
-version = "1.0"
+version = "1.0-0.13"
 
-# description = "Your plan to rule the world"
-# long_description = "More description about your plan"
-# author = "Your Name Here"
-# email = "YourEmail@YourDomain"
-# copyright = "Vintage 2006 - a good year indeed"
+description = "Loggerhead is a web viewer for projects in bazaar."
 
-# if it's open source, you might want to specify these
-# url = "http://yourcool.site/"
-# download_url = "http://yourcool.site/download"
-# license = "MIT"
+author = "Robey Pointer"
+email = "robey@lag.net"
+
+license = "GPL"
+url = "http://www.lag.net/loggerhead/"
+download_url = "http://www.lag.net/loggerhead/download/..."
