43764e33f2b8c5b7a87b0ffe06802c3a04f50dff
Robey Pointer <robey@lag.net>
2007-01-24 10:43:51 -0800

    bump up version number to 1.1 and change push site.

--- loggerhead/release.py
+++ loggerhead/release.py
@@ -1,6 +1,6 @@
 # Release information about loggerhead
 
-version = "1.0-0.13"
+version = "1.1-0.13"
 
 description = "Loggerhead is a web viewer for projects in bazaar."
 
@@ -9,4 +9,4 @@ email = "robey@lag.net"
 
 license = "GPL"
 url = "http://www.lag.net/loggerhead/"
-download_url = "http://www.lag.net/loggerhead/download/..."
+download_url = "http://www.lag.net/loggerhead/download/loggerhead-1.1-0.13.tar.gz"
