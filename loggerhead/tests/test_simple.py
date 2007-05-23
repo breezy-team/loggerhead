35c097e92780be328be811dd8ad3b0c396d3ecb7
Michael Hudson <michael.hudson@canonical.com>
2007-05-23 12:08:07 +0100

    infrastructure for less trivial tests no actual non trivial tests yet, though

--- loggerhead/tests/test_simple.py
+++ loggerhead/tests/test_simple.py
@@ -1,7 +1,13 @@
 from configobj import ConfigObj
+import unittest
+import os
+import tempfile
+import shutil
 
 import cherrypy
 from turbogears import testutil
+
+from bzrlib.bzrdir import BzrDir
 
 from loggerhead.controllers import Root
 
@@ -11,3 +17,38 @@ def test_simple():
     cherrypy.root = r
     testutil.create_request('/')
     assert 'loggerhead branches' in cherrypy.response.body[0]
+
+config_template = """
+[project]
+    [[branch]]
+        folder = '%s'
+"""
+
+
+class TestWithSimpleTree(object):
+    def setUp(self):
+        self.old_bzrhome = os.environ.get('BZR_HOME')
+        os.environ['BZR_HOME'] = ''
+        self.bzrbranch = tempfile.mkdtemp()
+        branch = BzrDir.create_branch_convenience(
+            self.bzrbranch, force_new_tree=True)
+        tree = branch.bzrdir.open_workingtree()
+        f = open(os.path.join(self.bzrbranch, 'file'), 'w')
+        f.write('data')
+        f.close()
+        tree.add('file')
+        tree.commit(message='.')
+
+        config = ConfigObj(config_template%self.bzrbranch)
+        cherrypy.root = Root(config)
+
+    def tearDown(self):
+        shutil.rmtree(self.bzrbranch)
+        if self.old_bzrhome is None:
+            del os.environ['BZR_HOME']
+        else:
+            os.environ['BZR_HOME'] = self.old_bzrhome
+
+    def test_index(self):
+        testutil.create_request('/')
+        assert 'loggerhead branches' in cherrypy.response.body[0]
