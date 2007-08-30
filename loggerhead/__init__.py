8b3711a92d64a782a34ab97a137036f3ed84675c
Michael Hudson <michael.hudson@canonical.com>
2007-08-30 19:53:13 +0100

    a bit tortuous, but fix the modified files links on the revision page

--- loggerhead/__init__.py
+++ loggerhead/__init__.py
@@ -8,18 +8,17 @@ def _pt(name):
     return zpt(os.path.join(_base, 'templates', name + '.pt'))
 
 
-def file_change_summary(url, entry, links=True):
+def file_change_summary(url, entry, modified_file_link):
     return _pt('revisionfilechanges')(
-        url=url, entry=entry, links=links,
+        url=url, entry=entry, modified_file_link=modified_file_link,
         **templatefunctions)
 
-def revisioninfo(url, branch, entry, includefilechanges=False):
+def revisioninfo(url, branch, entry, modified_file_link=None):
     from loggerhead import util
     return _pt('revisioninfo')(
         url=url, change=entry, branch=branch, util=util,
-        includefilechanges=includefilechanges,
+        modified_file_link=modified_file_link,
         **templatefunctions)
-
 
 def collapse_button(group, name, normal='block'):
     return _pt('collapse-button')(
@@ -38,8 +37,20 @@ def revno_with_nick(entry):
         extra = ''
     return '(%s%s)'%(entry.revno, extra)
 
+def modified_file_link_rev(url, entry, item):
+    return _pt('modified-file-link-rev')(
+        url=url, entry=entry, item=item,
+        **templatefunctions)
+
+def modified_file_link_log(url, entry, item):
+    return _pt('modified-file-link-log')(
+        url=url, entry=entry, item=item,
+        **templatefunctions)
+
 templatefunctions = {'file_change_summary': file_change_summary,
                      'revisioninfo': revisioninfo,
                      'collapse_button': collapse_button,
                      'collapse_all_button': collapse_all_button,
-                     'revno_with_nick': revno_with_nick}
+                     'revno_with_nick': revno_with_nick,
+                     'modified_file_link_log': modified_file_link_log,
+                     'modified_file_link_rev': modified_file_link_rev}
