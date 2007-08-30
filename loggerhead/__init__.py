63f66e556b50b670b899774d0642041c7148a9ba
Michael Hudson <michael.hudson@canonical.com>
2007-08-30 17:32:10 +0100

    tidyings, and efforts to not recompile some of the zpts each time they're used

--- loggerhead/__init__.py
+++ loggerhead/__init__.py
@@ -1,32 +1,31 @@
 import os
 import turbogears
 import turbozpt
-from turbozpt.template import PageTemplate
+from turbozpt.template import zpt
 
 _base = os.path.dirname(__file__)
 def _pt(name):
-    return PageTemplate(os.path.join(_base, 'templates', name + '.pt'))
+    return zpt(os.path.join(_base, 'templates', name + '.pt'))
 
 
 def file_change_summary(url, entry, links=True):
-    return _pt('revisionfilechanges').render(
-        dict(url=url, entry=entry, links=links))
+    return _pt('revisionfilechanges')(url=url, entry=entry, links=links)
 
 def revisioninfo(url, branch, entry, includefilechanges=False):
     from loggerhead import util
-    return _pt('revisioninfo').render(
-        dict(url=url, change=entry, branch=branch, util=util,
-             includefilechanges=includefilechanges,
-             file_change_summary=file_change_summary))
+    return _pt('revisioninfo')(
+        url=url, change=entry, branch=branch, util=util,
+        includefilechanges=includefilechanges,
+        file_change_summary=file_change_summary)
 
 
 def collapse_button(group, name, normal='block'):
-    return _pt('collapse-button').render(
-        dict(group=group, name=name, normal=normal, tg=turbogears))
+    return _pt('collapse-button')(
+        group=group, name=name, normal=normal, tg=turbogears)
 
 def collapse_all_button(group, normal='block'):
-    return _pt('collapse-all-button').render(
-        dict(group=group, normal=normal, tg=turbogears))
+    return _pt('collapse-all-button')(
+        group=group, normal=normal, tg=turbogears)
 
 templatefunctions = {'file_change_summary':file_change_summary,
                      'revisioninfo':revisioninfo,
--- turbozpt/template.py
+++ turbozpt/template.py
@@ -7,38 +7,44 @@ from zope.pagetemplate import pagetemplatefile
 import os.path
 import sys
 
+_zpt_cache = {}
+
+def _compile_template(tfile):
+    mod = PageTemplate(tfile)
+    mtime = os.stat(tfile).st_mtime
+    mod.__mtime__ = mtime
+    return mod
+
+def zpt(tfile):
+    if tfile in _zpt_cache:
+        mtime = os.stat(tfile).st_mtime
+        mod = _zpt_cache[tfile]
+        if mod.__mtime__ != mtime:
+            mod = _compile_template(tfile)
+            _zpt_cache[tfile] = mod
+    else:
+        mod = _zpt_cache[tfile] = _compile_template(tfile)
+    return mod
+
 class Here(object):
     def __init__(self, base, options):
         self.base = base
         self.options = options
 
     def __getitem__(self, name):
-        tpl = PageTemplate(os.path.join(self.base, name))
-        tpl.add_context(self.options)
-        return tpl
+        tpl = zpt(os.path.join(self.base, name))
+        return tpl(**self.options)
 
 class PageTemplate(pagetemplatefile.PageTemplateFile):
     def __init__(self, name):
     	base = os.path.dirname(sys._getframe(1).f_globals["__file__"])
-	self.extra_context = {}
         self.name = name
         self.fullpath = os.path.join(base, self.name)
         self.base = os.path.dirname(self.fullpath)
         pagetemplatefile.PageTemplateFile.__init__(self, self.fullpath)
-    
-    def render(self, extra_dict=None):
-	if extra_dict is None:
-            extra_dict = {}
-        context = self.pt_getContext()
-        context.update(extra_dict)
-	return self.pt_render(context)
-    
-    def add_context(self, d):
-	self.extra_context.update(d)
-	
+
     def pt_getContext(self, args=(), options={}, **ignored):
         rval = pagetemplatefile.PageTemplateFile.pt_getContext(self, args, options, **ignored)
 	rval.update(options)
-	rval.update(self.extra_context)
         rval.update({'here':Here(self.base, options), 'template':self})
         return rval
