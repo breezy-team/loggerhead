65d088e7e9231a858e7ba17c883a82d4480f5420
Michael Hudson <michael.hudson@canonical.com>
2008-04-15 10:40:03 +1200

    make daemonization less insane

--- loggerhead/daemon.py
+++ loggerhead/daemon.py
@@ -9,7 +9,6 @@ def daemonize(pidfile, home):
     background as a daemon.
     """
 
-    UMASK = 0
     WORKDIR = "/"
     REDIRECT_TO = getattr(os, 'devnull', '/dev/null')
     MAXFD = 1024
@@ -19,7 +18,7 @@ def daemonize(pidfile, home):
     except OSError, e:
         raise Exception("%s [%d]" % (e.strerror, e.errno))
 
-    if (pid == 0):      # The first child.
+    if pid == 0:      # The first child.
         os.setsid()
 
         try:
@@ -27,33 +26,32 @@ def daemonize(pidfile, home):
         except OSError, e:
             raise Exception, "%s [%d]" % (e.strerror, e.errno)
 
-        if (pid == 0):  # The second child.
+        if pid == 0:  # The second child.
             os.chdir(WORKDIR)
-            os.umask(UMASK)
         else:
             os._exit(0) # Exit parent (the first child) of the second child.
     else:
         os._exit(0)     # Exit parent of the first child.
 
-    #import resource            # Resource usage information.
-    #maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
-    #if (maxfd == resource.RLIM_INFINITY):
-    #    maxfd = MAXFD
+    import resource            # Resource usage information.
+    maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+    if (maxfd == resource.RLIM_INFINITY):
+        maxfd = MAXFD
 
     # Iterate through and close all file descriptors.
-    #for fd in range(3, maxfd):
-    #    try:
-    #        os.close(fd)
-    #    except OSError:        # ERROR, fd wasn't open to begin with (ignored)
-    #        pass
+    for fd in range(0, maxfd):
+        try:
+            os.close(fd)
+        except OSError:        # ERROR, fd wasn't open to begin with (ignored)
+            pass
 
     # This call to open is guaranteed to return the lowest file descriptor,
     # which will be 0 (stdin), since it was closed above.
-    #os.open(REDIRECT_TO, os.O_RDWR)    # standard input (0)
+    os.open(REDIRECT_TO, os.O_RDWR)    # standard input (0)
 
     # Duplicate standard input to standard output and standard error.
-    #os.dup2(0, 1)                      # standard output (1)
-    #os.dup2(0, 2)                      # standard error (2)
+    os.dup2(0, 1)                      # standard output (1)
+    os.dup2(0, 2)                      # standard error (2)
 
     f = open(pidfile, 'w')
     f.write('%d\n' % (os.getpid(),))
