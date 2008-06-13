ec4497588e0c7d3723d69e2deab6838959581825
Martin Albisetti <argentina@gmail.com>
2008-06-13 02:44:04 -0300

    * Make navigation links use revnos instead of revids  * Don't always pass on start_revid. Why would we do that?

--- loggerhead/util.py
+++ loggerhead/util.py
@@ -349,17 +349,21 @@ def fill_in_navigation(navigation):
 
     navigation.prev_page_revid = get_offset(-1 * navigation.pagesize)
     navigation.next_page_revid = get_offset(1 * navigation.pagesize)
+    prev_page_revno = navigation.branch._history.get_revno(
+            navigation.prev_page_revid)
+    next_page_revno = navigation.branch._history.get_revno(
+            navigation.next_page_revid)
 
     params = { 'filter_file_id': navigation.filter_file_id }
     if getattr(navigation, 'query', None) is not None:
         params['q'] = navigation.query
-    else:
-        params['start_revid'] = navigation.start_revid
 
     if navigation.prev_page_revid:
-        navigation.prev_page_url = navigation.branch.url([ navigation.scan_url, navigation.prev_page_revid ], **get_context(**params))
+        navigation.prev_page_url = navigation.branch.url([navigation.scan_url,
+            prev_page_revno], **params)
     if navigation.next_page_revid:
-        navigation.next_page_url = navigation.branch.url([ navigation.scan_url, navigation.next_page_revid ], **get_context(**params))
+        navigation.next_page_url = navigation.branch.url([navigation.scan_url,
+            next_page_revno], **params)
 
 
 def log_exception(log):
