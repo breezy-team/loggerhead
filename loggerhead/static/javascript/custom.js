5bd436aed1417164ff68a53017a5e7d6be709c96
Martin Albisetti <argentina@gmail.com>
2008-06-20 01:16:25 -0300

    * Show auto-suggest in a nicer more ajaxish way  * Switch again to mootools 1.2, I promise, this is the last time

--- loggerhead/static/javascript/custom.js
+++ loggerhead/static/javascript/custom.js
@@ -11,11 +11,30 @@ window.addEvent('domready', function()
         search: function(query)
         {
             url = '/bazaar/bzr_garbage/search?query=' + query;
-            new Ajax(url, {
-                            method: 'get',
-                            update: $('search_terms')
-                           }).request(); 
+	
+	        var posicion = search_box.getPosition();
+	        var size     = search_box.getSize();
+
+	        $('search_terms').setStyle('position','absolute');
+	        $('search_terms').setStyle('left',posicion.x);
+	        $('search_terms').setStyle('top',posicion.y + size.y);
+	        $('search_terms').setStyle('display','block');
+	        $('search_terms').set('html','Loading...');
+
+            new Request({'url':url,'method':'get','onComplete':cool_search}).send('');
+
+
         }
     });
 });
 
+function cool_search(response)
+{
+	var posicion = $('q').getPosition();
+	var size     = $('q').getSize();
+	$('search_terms').set('html',response);
+	$('search_terms').setStyle('display','block');
+	$('search_terms').setStyle('position','absolute');
+	$('search_terms').setStyle('left',posicion.x);
+	$('search_terms').setStyle('top',posicion.y + size.y);
+}
