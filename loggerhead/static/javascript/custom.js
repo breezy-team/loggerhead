dda2d96e3a7114b2c1b96a2dbcbc0dec4f001280
Martin Albisetti <argentina@gmail.com>
2008-06-25 13:48:43 -0300

    * Added icon for expanded view  * Triangle icon now rotates when expanding/contracting  * Search works now!  * Other random tweaks

--- loggerhead/static/javascript/custom.js
+++ loggerhead/static/javascript/custom.js
@@ -3,22 +3,25 @@ window.addEvent('domready', function()
     $$('.revision_log').each(function(item, i)
     {
         // FIXME: Make less redundant
-        var thisSlider = new Fx.Slide( item.getElement( '.revisioninfo' ), { duration: 200 } );
+        var expander = new Fx.Slide( item.getElement( '.revisioninfo' ), { duration: 200 } );
         var shortDescription = item.getElement( '.short_description' );
         var longDescription = item.getElement( '.long_description' );
-        thisSlider.hide();
+        var expand_icon = item.getElement( '.expand_icon' );
+        expander.hide();
         item.getElement( '.expand_revisioninfo' ).addEvent( 'click', function()
             {
-                thisSlider.toggle();
+                expander.toggle();
                 if(longDescription.style.display == 'none')
                 {
                     longDescription.style.display = 'block';
                     shortDescription.style.display = 'none';
+                    expand_icon.src = expand_icon.alt;
                 }
                 else
                 {
                     longDescription.style.display = 'none';
                     shortDescription.style.display = 'block';
+                    expand_icon.src = expand_icon.title;
                 }
             } );
     });
@@ -30,19 +33,19 @@ function toggle_expand_all(action)
     $$('.revision_log').each(function(item, i)
     {
         // FIXME: Make less redundant
-        var thisSlider = new Fx.Slide( item.getElement( '.revisioninfo' ), { duration: 200 } );
+        var expander = new Fx.Slide( item.getElement( '.revisioninfo' ), { duration: 200 } );
         var shortDescription = item.getElement( '.short_description' );
         var longDescription = item.getElement( '.long_description' );
         if(action == 'close')
         {
-            thisSlider.slideOut();
+            expander.slideOut();
             $('expand_all').style.display = 'block';
             $('collapse_all').style.display = 'none';
 
         }
         else if(action == 'open')
         {
-            thisSlider.slideIn();
+            expander.slideIn();
             $('expand_all').style.display = 'none';
             $('collapse_all').style.display = 'block';
         }
