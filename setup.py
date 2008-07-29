99cbf7838147271c21359a19457bf68cad048833
Martin Albisetti <argentina@gmail.com>
2008-07-29 17:49:41 -0300

    * Removed unused file  * Added copyright

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+#
+# Copyright (C) 2008  Canonical Ltd.
+#                     (Authored by Martin Albisetti <argentina@gmail.com>)
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
