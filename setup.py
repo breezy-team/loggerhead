26599787bca2d082612c8af3f516599cf65016f2
Martin Albisetti <argentina@gmail.com>
2008-07-31 01:16:57 -0300

    * Removed unused imports  * Removed checking for dependencies  * Removed unnecessary install class

--- setup.py
+++ setup.py
@@ -17,48 +17,11 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """Loggerhead is a web viewer for projects in bazaar"""
 
-from distutils.core import setup, Command
+from distutils.core import setup
 from distutils.command.install_data import install_data
-from distutils.dep_util import newer
-from distutils.log import info
-import glob
 import os
-import sys
 import loggerhead
-import bzrlib
 
-
-# Make sure you have all required dependencies
-if sys.version_info < (2, 4):
-    sys.stderr.write("[ERROR] Not a supported Python version. Need 2.4+\n")
-    sys.exit(1)
-
-bzrlib_version = bzrlib.version_info[:2]
-try:
-    from bzrlib.trace import warning
-except ImportError:
-    from warnings import warn as warning
-if bzrlib_version < loggerhead.required_bzrlib:
-    from bzrlib.errors import BzrError
-    warning('Installed Bazaar version %s is too old to be used with loggerhead'
-            ' %s.' % (bzrlib.__version__, __version__))
-    raise BzrError('Version mismatch: %r, %r' % (version_info, 
-                                                 bzrlib.version_info) )
-
-try:
-    import paste
-except ImportError:
-    raise errors.BzrCommandError("python-paste not installed.")
-try:
-    import simpletal
-except ImportError:
-    raise errors.BzrCommandError("python-simpletal not installed.")
-
-
-
-class InstallData(install_data):
-    def run(self):
-        install_data.run(self)
 
 setup(
     name = "loggerhead",
@@ -75,6 +38,6 @@ setup(
                 ('share/man/man1',['start-loggerhead.1', 'stop-loggerhead.1']),
                 ('share/doc/loggerhead', ['loggerhead.conf.example'])
                ],
-    cmdclass={'install_data':InstallData}
+    cmdclass={'install_data':install_data}
     )
     
