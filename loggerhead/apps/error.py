01144954711cd60faae9efc569b64b2e0389fb88
Michael Hudson <michael.hudson@canonical.com>
2008-09-30 10:35:53 +1300

    remove unused import

--- loggerhead/apps/error.py
+++ loggerhead/apps/error.py
@@ -15,7 +15,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-import logging
 from loggerhead.controllers.error_ui import ErrorUI
 
 class ErrorHandlerApp(object):
