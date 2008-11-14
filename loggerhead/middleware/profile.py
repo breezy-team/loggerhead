a1ba946bdd00a9f9feecfa894b6a8dd17502b653
Robert Collins <robertc@robertcollins.net>
2008-11-14 17:36:13 +1100

    Allow lsprofile data on requests to be gathered.

--- loggerhead/middleware/profile.py
+++ loggerhead/middleware/profile.py
@@ -1,67 +1,33 @@
 '''Profiling middleware for paste.'''
 import cgi
-import cProfile
-from cStringIO import StringIO
-import pstats
-import sys
+import threading
 
-from paste.debug.profile import ProfileMiddleware
-from paste import response
+from bzrlib.lsprof import profile
 
-class CProfileMiddleware(ProfileMiddleware):
-    '''Paste middleware for profiling with cProfile.'''
+class LSProfMiddleware(object):
+    '''Paste middleware for profiling with lsprof.'''
+
+    def __init__(self, app, global_conf=None):
+        self.app = app
+        self.lock = threading.Lock()
+        self.__count = 0
+
+    def __run_app(self, environ, start_response):
+        app_iter = self.app(environ, start_response)
+        try:
+            return [list(app_iter)]
+        finally:
+            if getattr(app_iter, 'close', None):
+                app_iter.close()
 
     def __call__(self, environ, start_response):
-
-        catch_response = []
-        body = []
-
-        def capture_output(func, *args, **kwargs):
-            out = StringIO()
-            old_stdout = sys.stdout
-            sys.stdout = out
-            try:
-                func(*args, **kwargs)
-            finally:
-                sys.stdout = old_stdout
-            return out.getvalue()
-
-        def replace_start_response(status, headers, exc_info=None):
-
-            catch_response.extend([status, headers])
-            start_response(status, headers, exc_info)
-            return body.append
-
-        def run_app():
-
-            app_iter = self.app(environ, replace_start_response)
-            try:
-                body.extend(app_iter)
-            finally:
-                if hasattr(app_iter, 'close'):
-                    app_iter.close()
-
+        """Run a request."""
         self.lock.acquire()
         try:
-            cProfile.runctx("run_app()", globals(), locals(),
-                filename='loggerhead.cprof')
-
-            body = ''.join(body)
-            headers = catch_response[1]
-            content_type = response.header_value(headers, 'content-type')
-            if content_type is None or \
-                not content_type.startswith('text/html') :
-                # We can't add info to non-HTML output
-                return [body]
-            stats = pstats.Stats('loggerhead.cprof')
-            #stats.sort_stats('time', 'calls')
-            stats.strip_dirs()
-            output = capture_output(stats.print_stats, self.limit)
-            output_callers = capture_output(
-                stats.print_callers, self.limit)
-            body += '<pre style="%s">%s\n%s</pre>' % (
-                self.style, cgi.escape(output), cgi.escape(output_callers))
-            return [body]
+            ret, stats = profile(self.__run_app, environ, start_response)
+            self.__count += 1
+            stats.save("%d-stats.callgrind" % self.__count, format="callgrind")
+            return ret
         finally:
             self.lock.release()
 
