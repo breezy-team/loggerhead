3aa370e2a90c9fe2f212286b8651e2812bbd9169
Robert Collins <robertc@robertcollins.net>
2008-12-05 16:07:33 +1100

    Load all bzrlib plugins.

--- loggerhead/apps/__init__.py
+++ loggerhead/apps/__init__.py
@@ -4,6 +4,8 @@ import os
 
 from paste import urlparser, fileapp
 
+from bzrlib.plugin import load_plugins
+
 static = os.path.join(
     os.path.dirname(os.path.dirname(__file__)), 'static')
 
@@ -11,3 +13,6 @@ static_app = urlparser.make_static(None, static)
 
 favicon_app = fileapp.FileApp(os.path.join(static, 'images', 'favicon.ico'))
 robots_app = fileapp.FileApp(os.path.join(static, 'robots.txt'))
+
+# load plugins - such as svn:// support, extra formats and so on.
+load_plugins()
