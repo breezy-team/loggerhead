16d95fcd8cbd78821cea7140217da1132d7d4230
John Arbash Meinel <john@arbash-meinel.com>
2010-05-12 09:56:43 -0500

    Make a note why we restrict simpleTAL logging, and make it not *increase* verbosity.

--- loggerhead/zptsupport.py
+++ loggerhead/zptsupport.py
@@ -23,8 +23,14 @@ import StringIO
 
 from simpletal import simpleTAL, simpleTALES
 
-logging.getLogger("simpleTAL").setLevel(logging.INFO)
-logging.getLogger("simpleTALES").setLevel(logging.INFO)
+def _restrict_logging(logger_name):
+    logger = logging.getLogger(logger_name)
+    if logger.getEffectiveLevel() < logging.INFO:
+        logger.setLevel(logging.INFO)
+# simpleTAL is *very* verbose in DEBUG mode, which is otherwise the default. So
+# quiet it up a bit.
+_restrict_logging('simpleTAL')
+_restrict_logging('simpleTALES')
 
 
 _zpt_cache = {}
