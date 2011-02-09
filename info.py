737d2af774aa3ed6cc8f46d8fe4833a3d213ae92
John Arbash Meinel <john@arbash-meinel.com>
2011-02-09 16:33:14 -0600

    Make loggerhead compatible with bzr.dev again

--- info.py
+++ info.py
@@ -6,7 +6,8 @@ bzr_plugin_name = "loggerhead"
 bzr_plugin_version = (1, 18, 0)
 
 bzr_compatible_versions = [
-    (1, 17, 0), (1, 18, 0), (2, 0, 0), (2, 1, 0), (2, 2, 0), (2, 3, 0)]
+    (1, 17, 0), (1, 18, 0), (2, 0, 0), (2, 1, 0), (2, 2, 0), (2, 3, 0),
+    (2, 4, 0)]
 
 bzr_minimum_version = bzr_compatible_versions[0]
 
