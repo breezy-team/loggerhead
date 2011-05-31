d8877716b13cc272475a9291a0bd78c7626c0628
geoffreyfishing@gmail.com <geoffreyfishing@gmail.com>
2011-05-30 22:06:21 -0500

    Cleaned up exporter.

--- loggerhead/exporter.py
+++ loggerhead/exporter.py
@@ -1,12 +1,9 @@
 
 import tarfile
-import StringIO
 
 from bzrlib.export import _export_iter_entries
 from bzrlib import osutils
 from bzrlib import errors
-from bzrlib.filters import ContentFilterContext
-from bzrlib.filters import filtered_output_bytes
 
 class TarExporterFileObject(object):
     
@@ -31,12 +28,11 @@ def export_tarball(history, revid):
     
     root = None
     subdir = None
-    filtered = False
     force_mtime = None
     
-    fileobj = TarExporterFileObject()
+    tarfileobj = TarExporterFileObject()
     
-    ball = tarfile.open(None, 'w:gz', fileobj)
+    ball = tarfile.open(None, 'w:gz', tarfileobj)
     tree = history._branch.repository.revision_tree(revid)
     
     #TODO: remove unnecessary code
@@ -54,17 +50,8 @@ def export_tarball(history, revid):
                 item.mode = 0755
             else:
                 item.mode = 0644
-            if filtered:
-                chunks = tree.get_file_lines(ie.file_id)
-                filters = tree._content_filter_stack(dp)
-                context = ContentFilterContext(dp, tree, ie)
-                contents = filtered_output_bytes(chunks, filters, context)
-                content = ''.join(contents)
-                item.size = len(content)
-                fileobj = StringIO.StringIO(content)
-            else:
-                item.size = tree.get_file_size(ie.file_id)
-                fileobj = tree.get_file(ie.file_id)
+            item.size = tree.get_file_size(ie.file_id)
+            fileobj = tree.get_file(ie.file_id)
         elif ie.kind == "directory":
             item.type = tarfile.DIRTYPE
             item.name += '/'
@@ -81,7 +68,7 @@ def export_tarball(history, revid):
             raise errors.BzrError("don't know how to export {%s} of kind %r" %
                            (ie.file_id, ie.kind))
         ball.addfile(item, fileobj)
-        yield fileobj.get_buffer()
+        yield tarfileobj.get_buffer()
 
         
         
