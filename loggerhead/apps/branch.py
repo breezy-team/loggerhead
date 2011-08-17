af8b9e42f6e558051c25bef23cf2b11ea8cdadb4
Danilo Segan <danilo@canonical.com>
2011-08-17 13:04:00 +0200

    Get rid of FileChangeCache and its uses.

--- loggerhead/apps/branch.py
+++ loggerhead/apps/branch.py
@@ -63,23 +63,20 @@ class BranchWSGIApp(object):
         self.use_cdn = use_cdn
 
     def get_history(self):
-        file_cache = None
         revinfo_disk_cache = None
         cache_path = self._config.get('cachepath', None)
         if cache_path is not None:
             # Only import the cache if we're going to use it.
             # This makes sqlite optional
             try:
-                from loggerhead.changecache import (
-                    FileChangeCache, RevInfoDiskCache)
+                from loggerhead.changecache import RevInfoDiskCache
             except ImportError:
                 self.log.debug("Couldn't load python-sqlite,"
                                " continuing without using a cache")
             else:
-                file_cache = FileChangeCache(cache_path)
                 revinfo_disk_cache = RevInfoDiskCache(cache_path)
         return History(
-            self.branch, self.graph_cache, file_cache=file_cache,
+            self.branch, self.graph_cache,
             revinfo_disk_cache=revinfo_disk_cache, cache_key=self.friendly_name)
 
     def url(self, *args, **kw):
