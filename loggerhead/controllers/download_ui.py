1c509cddc8eb34897c97a473fba0fdc4e38a69fe
Tom Wardill <tom.wardill@canonical.com>
2020-07-08 17:11:10 +0000

    Add encode for py3 compat

--- loggerhead/controllers/download_ui.py
+++ loggerhead/controllers/download_ui.py
@@ -59,7 +59,7 @@ class DownloadUI (TemplatedBranchView):
                 self._branch.absolute_url('/changes'))
         revid = h.fix_revid(args[0])
         try:
-            path, filename, content = h.get_file(args[1], revid)
+            path, filename, content = h.get_file(args[1].encode('UTF-8'), revid)
         except (NoSuchFile, NoSuchRevision):
             # Compatibility API for /download/rev_id/file_id/<filename>
             try:
